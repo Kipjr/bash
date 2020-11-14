ded214370e11752435d35844dedc540411a17b1d
Martie <martijn9873@hotmail.com>
2020-11-14 14:40:12 +0100

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-var=$(date +"%FORMAT_STRING")
-now=$(date +"%m_%d_%Y")
-printf "%s\n" $now
-today=$(date +"%Y-%m-%d")
-tar -cf archive_$today.tar /home/pi/  # Create archive.tar from files foo and bar.
+DATE=$(date +%Y-%m-%d)
+BACKUP_DIR=”/home/pi/backup”
+STORE_DIR=”/home/pi/data”
+tar -zcvpf $STORE_DIR/Archive-$DATE.tar.gz $BACKUP_DIR # Create archive.tar from files foo and bar.
+
+# Delete files older than 10 days #
+find $BACKUP_DIR/* -mtime +10 -exec rm {} \;
+
+
+
+
+
